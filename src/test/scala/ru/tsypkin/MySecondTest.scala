97e4a29f7f160b46c9d1c9c8d8d59e7ac4afa065
AntonTsypkin <Cypkinanthon>
2015-03-22 03:47:37 +0400

    init commit

--- src/test/scala/ru/tsypkin/MySecondTest.scala
+++ src/test/scala/ru/tsypkin/MySecondTest.scala
@@ -9,7 +9,7 @@ class MyClassForTest { val ttt = 1
 
 class MySecondTest extends FunSuite with Matchers {
   test("my second test 1") {
-    val x: Int = 7
+    val x: Int = 8
     val y = SingletonObject.toString
     println(SingletonObject.myFunction(1, y))
     val sub: SubPack = new SubPack("y")
