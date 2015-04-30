81afb00d99e91431e7fa1b8ef812404c563c5a41
AntonTsypkin <Cypkinanthon>
2015-04-30 16:39:34 +0400

    readNumber

--- src/main/scala/ru/tsypkin/HelloWorld.scala
+++ src/main/scala/ru/tsypkin/HelloWorld.scala
@@ -9,7 +9,7 @@ object SingletonObject extends ReadNumber with PascalV2 with PascalV1 with ReadN
   def main(args: Array[String]): Unit = {
     //readNumber()
     //readNumberV2("tr566979 5ty 67 89 00077")
-    //readNomberMatching()
+    readNomberMatching()
     //decorate("HAllo" ,"Ага ")
     //createMassive ()
     //createMassiveReserch()
