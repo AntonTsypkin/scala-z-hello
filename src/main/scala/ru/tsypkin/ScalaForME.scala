a6e4d3b9d1cc31b3040568fef4c1463c85ff5ead
AntonTsypkin <Cypkinanthon>
2015-09-02 13:02:31 +0400

    readNumber

--- src/main/scala/ru/tsypkin/ScalaForME.scala
+++ src/main/scala/ru/tsypkin/ScalaForME.scala
@@ -7,12 +7,11 @@ trait ScalaForME {
    def olimpicExample1 (): Unit = {
 val myArrayNomber = Array[Int](1,2,3,4,5)
      val myNomberLength = 4
-     val myFactorials = new Array[Int](myNomberLength)
+     val myFactorials = new Array[Int](5)
      var x = 0
      for (x <- 0 to myNomberLength)
        if (x != 0) {
          myFactorials(x) = myFactorials(x-1) + myArrayNomber(x)
-         x.+(1)
        }
      myFactorials(0) = 1
      myFactorials.foreach(println)
