9af69e5af90b84ce4208a7e8ce4b3f9442db7c06
AntonTsypkin <Cypkinanthon>
2015-09-10 20:17:27 +0400

    readNumber

--- src/main/scala/ru/tsypkin/ScalaForME.scala
+++ src/main/scala/ru/tsypkin/ScalaForME.scala
@@ -6,7 +6,7 @@ package ru.tsypkin
 trait ScalaForME {
    def interData (): Unit = {
      val myArrayLenght = 11 // длина числового ряда
-     val myX = 5// искомое число
+     val myX = 49// искомое число
      println("Искомый результат " + myX)
      val myArrayNomber = new Array[Int](myArrayLenght) //мой числовой ряд
      for (y <- 0 to (myArrayLenght - 1)) myArrayNomber(y) = y //здесь явно можно сделать проще!!!
@@ -37,6 +37,28 @@ trait ScalaForME {
          //y = 0
 
      for (i <- 0 to saveIndexForMinus.length - 1) if (saveIndexForMinus(i) != 0) println("Поставить минус перед цифрой " + saveIndexForMinus(i))//вывод чисел перед которыми нужно поставить минус
+     //далее выстраиваем итоговый ряд из цифр и знаков
+     //сначала превратим saveIndex в последовательность минусов и плюсов
+     val totalSignature = new Array [String] (saveIndexForMinus.length)
+     for (i <- 0 to totalSignature.length - 1) if (saveIndexForMinus(i) == 0) totalSignature(i) = "+" else totalSignature(i) = "-"
+     val totalString = new Array[String](myArrayNomber.length + totalSignature.length)
+     totalString(0) = totalSignature(0)
+     totalString(1) = myArrayNomber(0).toString
+     var indexNomero = 1
+     var indexSignature = 1
+     for (i <- 2 to totalString.length - 1) {
+       if (i%2 == 0) {
+         totalString(i) = myArrayNomber(indexNomero).toString
+         indexNomero.+(1)
+       }
+       else {
+         totalString(i) = totalSignature(indexSignature)
+         indexSignature.+(1)
+       }
+     }
+
+
    }
+
 }
 
